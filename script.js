a1d0447ad3c4dec6fa4c865a30089ac8825a4b89
Gopi K <gopinathkk@live.com.sg>
2022-06-17 19:51:05 +1000

    added function to retrive stored data from local storage, and also to reset storage at the end of day

--- script.js
+++ script.js
@@ -2,48 +2,70 @@ var now = moment();
 var date = document.querySelector("#date");
 var day = document.querySelector("#day");
 var event1 = document.querySelector("#event1");
-// var id2 =document.querySelector("#2")
 var hour = moment().format("HH");
-var events = ["", "", "", "", "", "", "", "", "", ""];
-date.innerHTML = moment().format("MMMM Do YYYY, h:mm:ss a");
-//day.textContent = $("#event0").val(); //moment().format('dddd');
-var btn0 = document.querySelector("#button0")
-btn0.addEventListener("click",function(event){eventStore();})
-var btn1 = document.querySelector("#button1")
-btn1.addEventListener("click",function(event){eventStore()})
-var btn2 = document.querySelector("#button2")
-btn2.addEventListener("click",function(event){eventStore()})
-var btn3 = document.querySelector("#button3")
-btn3.addEventListener("click",function(event){eventStore()})
-var btn4 = document.querySelector("#button4")
-btn4.addEventListener("click",function(event){eventStore()})
-var btn5 = document.querySelector("#button5")
-btn5.addEventListener("click",function(event){eventStore()})
-var btn6 = document.querySelector("#button6")
-btn6.addEventListener("click",function(event){eventStore()})
-var btn7 = document.querySelector("#button7")
-btn7.addEventListener("click",function(event){eventStore()})
-var btn8 = document.querySelector("#button8")
-btn8.addEventListener("click",function(event){eventStore()})
-var btn9 = document.querySelector("#button9")
-btn9.addEventListener("click",function(event){eventStore()})
-var btn10 = document.querySelector("#button10")
-btn10.addEventListener("click",function(event){eventStore()})
+var events = ["", "", "", "", "", "", "", "", "", "","",""];
+date.innerHTML = moment().format("Do MMMM YYYY");
+day.innerHTML=moment().format("dddd")
+var btn0 = document.querySelector("#button0");
+btn0.addEventListener("click", function () {
+  eventStore();
+});
+var btn1 = document.querySelector("#button1");
+btn1.addEventListener("click", function () {
+  eventStore();
+});
+var btn2 = document.querySelector("#button2");
+btn2.addEventListener("click", function () {
+  eventStore();
+});
+var btn3 = document.querySelector("#button3");
+btn3.addEventListener("click", function () {
+  eventStore();
+});
+var btn4 = document.querySelector("#button4");
+btn4.addEventListener("click", function () {
+  eventStore();
+});
+var btn5 = document.querySelector("#button5");
+btn5.addEventListener("click", function () {
+  eventStore();
+});
+var btn6 = document.querySelector("#button6");
+btn6.addEventListener("click", function () {
+  eventStore();
+});
+var btn7 = document.querySelector("#button7");
+btn7.addEventListener("click", function () {
+  eventStore();
+});
+var btn8 = document.querySelector("#button8");
+btn8.addEventListener("click", function () {
+  eventStore();
+});
+var btn9 = document.querySelector("#button9");
+btn9.addEventListener("click", function () {
+  eventStore();
+});
+var btn10 = document.querySelector("#button10");
+btn10.addEventListener("click", function () {
+  eventStore();
+});
 
-function eventStore(){
- events[0]=$("#event0").val();
-events[1]=$("#event1").val();
-events[2]=$("#event2").val();
-events[3]=$("#event3").val();
-events[4]=$("#event4").val();
-events[5]=$("#event5").val();
-events[6]=$("#event6").val();
-events[7]=$("#event7").val();
-events[8]=$("#event8").val();
-events[9]=$("#event9").val();
-localStorage.setItem("dailyEvents", JSON.stringify(events)); 
-  
-};
+function eventStore() {
+  events[0] = $("#event0").val();
+  events[1] = $("#event1").val();
+  events[2] = $("#event2").val();
+  events[3] = $("#event3").val();
+  events[4] = $("#event4").val();
+  events[5] = $("#event5").val();
+  events[6] = $("#event6").val();
+  events[7] = $("#event7").val();
+  events[8] = $("#event8").val();
+  events[9] = $("#event9").val();
+  events[10]=$("#event10").val();
+  events[11]=moment().format("DDMMYYYY")
+  localStorage.setItem("dailyEvents", JSON.stringify(events));
+}
 
 function labelColor() {
   var hourNumber = hour - 8;
@@ -177,7 +199,53 @@ function labelColor() {
     $("#event9").removeClass("list-group-item-danger");
     $("#event9").addClass("list-group-item-dark");
   }
+  if (hourNumber == 10) {
+    $("#event10").removeClass("list-group-item-success");
+    $("#event10").removeClass("list-group-item-dark");
+    $("#event10").addClass("list-group-item-danger");
+  } else if (hourNumber < 10) {
+    $("#event10").removeClass("list-group-item-danger");
+    $("#event10").removeClass("list-group-item-dark");
+    $("#event10").addClass("list-group-item-success");
+  } else {
+    $("#event10").removeClass("list-group-item-success");
+    $("#event10").removeClass("list-group-item-danger");
+    $("#event10").addClass("list-group-item-dark");
+  }
+}
+
+function eventRetriev(){
+  events=JSON.parse(localStorage.getItem("dailyEvents"));
+  if(events[11]==moment().format("DDMMYYYY")){
+  $("#event0").val(events[0])
+  $("#event1").val(events[1])
+  $("#event2").val(events[2])
+  $("#event3").val(events[3])
+  $("#event4").val(events[4])
+  $("#event5").val(events[5])
+  $("#event6").val(events[6])
+  $("#event7").val(events[7])
+  $("#event8").val(events[8])
+  $("#event9").val(events[9])
+  $("#event10").val(events[10])
+}
+else{
+  events=["", "", "", "", "", "", "", "", "", "","",""]
+  localStorage.setItem("dailyEvents", JSON.stringify(events));
+  $("#event0").val(events[0])
+  $("#event1").val(events[1])
+  $("#event2").val(events[2])
+  $("#event3").val(events[3])
+  $("#event4").val(events[4])
+  $("#event5").val(events[5])
+  $("#event6").val(events[6])
+  $("#event7").val(events[7])
+  $("#event8").val(events[8])
+  $("#event9").val(events[9])
+  $("#event10").val(events[10])
+}
+
 }
 
 labelColor();
-
+eventRetriev();
